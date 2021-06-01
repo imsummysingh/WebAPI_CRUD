aed0e1f6488451ef16b8ffbb02e5e0a54862a7e4
Summy Singh <summysingh86@gmail.com>
2021-06-01 15:55:34 +0530

    added

--- WebAPICRUD/EmployeeService/Controllers/EmployeesController.cs
+++ WebAPICRUD/EmployeeService/Controllers/EmployeesController.cs
@@ -17,12 +17,144 @@ namespace EmployeeService.Controllers
                 return entities.Employees.ToList();
             }
         }
-        public Employee Get(int id)
+        //public Employee Get(int id)
+        //{
+        //    using (EmployeeDBEntities entities = new EmployeeDBEntities())
+        //    {
+        //        return entities.Employees.FirstOrDefault(e => e.ID == id);
+        //    }
+        //}
+
+        public HttpResponseMessage Get(int id)
         {
-            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+
+                    if (entity != null)
+                    {
+                        return Request.CreateResponse(HttpStatusCode.OK, entity);
+                    }
+                    else
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with Id = " + id.ToString() + " not found");
+                    }
+                }            
+        }
+
+        //data can be from header, uri, and body
+        //we tell them the data is going to come from body
+
+        //public void Post([FromBody] Employee employee)
+        //{
+        //    using (EmployeeDBEntities entities = new EmployeeDBEntities())
+        //    {
+        //        entities.Employees.Add(employee);
+        //        entities.SaveChanges();
+        //    }
+        //}
+
+        public HttpResponseMessage Post([FromBody] Employee employee)
+        {
+            try
             {
-                return entities.Employees.FirstOrDefault(e => e.ID == id);
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    entities.Employees.Add(employee);
+                    entities.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                    message.Headers.Location = new Uri(Request.RequestUri + employee.ID.ToString());
+
+                    return message;
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadGateway, ex);
             }
         }
+
+        //public void Delete(int id)
+        //{
+        //    using(EmployeeDBEntities entities = new EmployeeDBEntities())
+        //    {
+        //        entities.Employees.Remove(entities.Employees.FirstOrDefault(e => e.ID == id));
+        //        entities.SaveChanges();
+        //    }
+        //}
+
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+                    if (entity != null)
+                    {
+                        entities.Employees.Remove(entity);
+                        entities.SaveChanges();
+
+                        return Request.CreateResponse(HttpStatusCode.OK);
+                    }
+                    else
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with Id = " + id.ToString() + " not found");
+                    }
+                }
+            }catch(Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+            
+        }
+
+        //public void Put(int id, [FromBody] Employee employee)
+        //{
+        //    using (EmployeeDBEntities entities = new EmployeeDBEntities())
+        //    {
+        //        var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+
+        //        entity.FirstName = employee.FirstName;
+        //        entity.LastName = employee.LastName;
+        //        entity.Gender = employee.Gender;
+        //        entity.Salary = employee.Salary;
+
+        //        entities.SaveChanges();
+        //    }
+        //}
+
+        public HttpResponseMessage Put(int id, [FromBody] Employee employee)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+
+                    if (entity != null)
+                    {
+                        entity.FirstName = employee.FirstName;
+                        entity.LastName = employee.LastName;
+                        entity.Gender = employee.Gender;
+                        entity.Salary = employee.Salary;
+
+                        entities.SaveChanges();
+
+                        return Request.CreateResponse(HttpStatusCode.OK);
+                    }
+                    else
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with Id = " + id.ToString() + " not found");
+                    }                    
+                }
+            }catch(Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadGateway,ex);
+            }
+            
+        }
+
     }
 }
